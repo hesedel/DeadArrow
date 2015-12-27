981c54f3e61ed5a0fb8a0c64fb5de9d1f0a3d7dc
Hesedel <hes@pajaroncreative.com>
2015-12-28 00:50:11 +0100

    4

--- DeadArrow/Bow.swift
+++ DeadArrow/Bow.swift
@@ -13,9 +13,11 @@ class Bow:SKShapeNode {
     var width:CGFloat = 0.0
     var width_2:CGFloat = 0.0
     var height:CGFloat = 0.0
+    var maxDrawDistance:CGFloat = 0.0
     var reusablePath = CGPathCreateMutable()
     var bow = SKShapeNode()
     var string = SKShapeNode()
+    var arrow = SKShapeNode()
     var anchor = SKShapeNode()
     var left = SKShapeNode()
     var right = SKShapeNode()
@@ -33,16 +35,23 @@ class Bow:SKShapeNode {
         self.width = self.baseUnit * 2
         self.width_2 = self.baseUnit
         self.height = self.baseUnit / 3
+        self.maxDrawDistance = self.width_2 - self.height
         
+        self.reusablePath = CGPathCreateMutable()
+        CGPathMoveToPoint(self.reusablePath, nil, 0, self.maxDrawDistance)
+        CGPathAddLineToPoint(self.reusablePath, nil, 0.0, -self.height)
+        self.arrow.path = self.reusablePath
         self.anchor = SKShapeNode(circleOfRadius:self.lineWidth)
         self.left = SKShapeNode(circleOfRadius:self.lineWidth)
         self.right = SKShapeNode(circleOfRadius:self.lineWidth)
         
+        self.arrow.hidden = true;
         self.left.position = CGPoint(x:-self.width_2, y:-self.height)
         self.right.position = CGPoint(x:self.width_2, y:-self.height)
         
         self.bow.lineWidth = self.lineWidth
         self.bow.strokeColor = UIColor.blackColor()
+        self.arrow.strokeColor = UIColor.blackColor()
         self.string.strokeColor = UIColor.blackColor()
         self.anchor.fillColor = UIColor.redColor()
         self.anchor.strokeColor = UIColor.blackColor()
@@ -53,8 +62,9 @@ class Bow:SKShapeNode {
         
         self.drawBow()
         
-        self.addChild(self.bow)
         self.addChild(self.string)
+        self.addChild(self.bow)
+        self.addChild(self.arrow)
         self.addChild(self.anchor)
         self.addChild(self.left)
         self.addChild(self.right)
@@ -65,7 +75,8 @@ class Bow:SKShapeNode {
     }
     
     func drawBow(var drawDistance:CGFloat = 0.0) {
-        drawDistance = [0, drawDistance - self.height].maxElement()!
+        drawDistance = [0.0, drawDistance - self.height].maxElement()!
+        drawDistance = [self.maxDrawDistance, drawDistance].minElement()!
         let drawDistance_4 = drawDistance / 4
         let newWidth_2 = self.width_2 - drawDistance_4
         let newHeight = self.height + drawDistance_4
@@ -75,6 +86,9 @@ class Bow:SKShapeNode {
         CGPathAddCurveToPoint(self.reusablePath, nil, -newWidth_2, -newHeight, -newWidth_2, 0.0, 0.0, 0.0)
         CGPathAddCurveToPoint(self.reusablePath, nil, newWidth_2, 0.0, newWidth_2, -newHeight, newWidth_2, -newHeight)
         self.bow.path = self.reusablePath
+        
+        self.arrow.hidden = drawDistance == 0.0
+        self.arrow.position.y = -drawDistance
         
         self.reusablePath = CGPathCreateMutable()
         CGPathMoveToPoint(self.reusablePath, nil, -newWidth_2, -newHeight)
--- DeadArrow/GameScene2.swift
+++ DeadArrow/GameScene2.swift
@@ -9,13 +9,13 @@
 import SpriteKit
 
 class GameScene2: SKScene {
-    //let bowAnchor = SKShapeNode(circleOfRadius:10.0)
-    //let bow = SKShapeNode()
-    var bow = Bow()
-    let finger1 = SKShapeNode(circleOfRadius:10.0)
-    let line = SKShapeNode()
     var baseUnit:CGFloat = 0.0
     var reusablePath = CGPathCreateMutable()
+    var bow = Bow()
+    var bowDrawingZone = SKShapeNode()
+    var bowDrawingZoneYBottom:CGFloat = 0.0
+    var fingerHasCrossedBowDrawingZone = false
+    var arrows = [SKShapeNode]()
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
@@ -23,71 +23,99 @@ class GameScene2: SKScene {
         //myLabel.text = "Hello, World!"
         //myLabel.fontSize = 45
         //myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        
         self.baseUnit = CGRectGetMaxX(self.frame) / 12
+        
         self.bow = Bow(baseUnit: self.baseUnit)
+        self.bowDrawingZone = SKShapeNode(rectOfSize:CGSize(width:self.bow.width, height:self.bow.height))
+        
+        self.bowDrawingZone.userInteractionEnabled = true
+        
         self.bow.position = CGPoint(x:CGRectGetMidX(self.frame), y:(CGRectGetMidY(self.frame) / 2))
-        self.finger1.position = CGPoint(x:self.bow.position.x, y:(self.bow.position.y - self.bow.height))
-        self.finger1.fillColor = UIColor.blueColor()
-        self.finger1.strokeColor = UIColor.blackColor()
-        self.line.strokeColor = UIColor.blackColor()
+        self.bowDrawingZone.position = CGPoint(x:self.bow.position.x, y:(self.bow.position.y - (self.bow.height / 2)))
+        
+        self.bowDrawingZoneYBottom = CGRectGetMinY(self.bowDrawingZone.frame)
+        
+        self.bowDrawingZone.fillColor = UIColor.magentaColor()
+        self.bowDrawingZone.lineWidth = 0.0
         
         //self.addChild(myLabel)
+        self.addChild(self.bowDrawingZone)
         self.addChild(self.bow)
-        self.addChild(self.line)
-        self.addChild(self.finger1)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         
-        for touch in touches {
-            let location = touch.locationInNode(self)
+        //for touch in touches {
+            //let location = touch.locationInNode(self)
             
             //let sprite = SKSpriteNode(imageNamed:"Spaceship")
             
             //sprite.xScale = 0.5
             //sprite.yScale = 0.5
             //sprite.position = location
-            self.finger1.position = location
-            self.reusablePath = CGPathCreateMutable()
-            CGPathMoveToPoint(self.reusablePath, nil, self.bow.position.x, self.bow.position.y)
-            CGPathAddLineToPoint(self.reusablePath, nil, location.x, location.y)
-            self.line.path = self.reusablePath
-            
-            self.updateBow()
             
             //let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
             
             //sprite.runAction(SKAction.repeatActionForever(action))
             
             //self.addChild(sprite)
-        }
+        //}
     }
     
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        for touch in touches {
+        if let touch = touches.first {
             let location = touch.locationInNode(self)
             
-            self.finger1.position = location
-            self.reusablePath = CGPathCreateMutable()
-            CGPathMoveToPoint(self.reusablePath, nil, self.bow.position.x, self.bow.position.y)
-            CGPathAddLineToPoint(self.reusablePath, nil, location.x, location.y)
-            self.line.path = self.reusablePath
+            if (CGRectContainsPoint(self.bowDrawingZone.frame, location)) {
+                self.fingerHasCrossedBowDrawingZone = true;
+            }
             
-            self.updateBow()
+            if (!self.fingerHasCrossedBowDrawingZone || location.y >= self.bowDrawingZoneYBottom) {
+                return
+            }
+            
+            self.bow.zRotation = atan2(location.y - self.bow.position.y, location.x - self.bow.position.x) + CGFloat(M_PI_2)
+            
+            let drawDistanceX = self.bow.position.x - location.x
+            let drawDistanceY = self.bow.position.y - location.y
+            let drawDistance = sqrt((drawDistanceX * drawDistanceX) + (drawDistanceY * drawDistanceY))
+            
+            self.bow.drawBow(drawDistance)
+        }
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first {
+            if (!self.fingerHasCrossedBowDrawingZone) {
+                return
+            }
+            
+            let location = touch.locationInNode(self)
+            
+            self.bow.drawBow()
+        
+            let distanceX = self.bow.position.x - location.x
+            let distanceY = self.bow.position.y - location.y
+        
+            let arrow = SKShapeNode()
+            arrow.path = self.bow.arrow.path
+            arrow.position = self.bow.position
+            arrow.zRotation = self.bow.zRotation
+            arrow.strokeColor = UIColor.blackColor()
+            
+            let action = SKAction.moveBy(CGVector(dx:distanceX, dy:distanceY), duration:0.125)
+            arrow.runAction(SKAction.repeatActionForever(action))
+            
+            self.arrows.append(arrow)
+            self.addChild(arrow)
+            
+            self.fingerHasCrossedBowDrawingZone = false;
         }
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
-    }
-    
-    func updateBow() {
-        let distanceX = self.bow.position.x - self.finger1.position.x
-        let distanceY = self.bow.position.y - self.finger1.position.y
-        let drawDistance = sqrt((distanceX * distanceX) + (distanceY * distanceY))
-        
-        self.bow.zRotation = atan2(self.finger1.position.y - self.bow.position.y, self.finger1.position.x - self.bow.position.x) + CGFloat(M_PI_2)
-        self.bow.drawBow(drawDistance)
     }
 }
